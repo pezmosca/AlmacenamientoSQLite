a8763e149418518eac52fd600f16821cc45ab25b
pezmosca <toniarellano96@gmail.com>
2016-02-02 13:16:42 +0100

    Initial commit

---
+++ app/src/main/java/com/example/toni/almacenamientosqlite/MainActivity.java
@@ -0,0 +1,116 @@
+package com.example.toni.almacenamientosqlite;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText et1, et2, et3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et1 = (EditText) findViewById(R.id.etcodigo);
+        et2 = (EditText) findViewById(R.id.etdescripcion);
+        et3 = (EditText) findViewById(R.id.etprecio);
+    }
+
+    public void alta(View v) {
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "administracion", null, 1);
+        SQLiteDatabase bd = admin.getWritableDatabase();
+        String cod = et1.getText().toString();
+        String descri = et2.getText().toString();
+        String pre = et3.getText().toString();
+        ContentValues registro = new ContentValues();
+        registro.put("codigo", cod);
+        registro.put("descripcion", descri);
+        registro.put("precio", pre);
+        bd.insert("articulos", null, registro);
+        bd.close();
+        et1.setText("");
+        et2.setText("");
+        et3.setText("");
+        Toast.makeText(this, "Se cargaron los datos del articulo", Toast.LENGTH_SHORT).show();
+    }
+
+    public void consultaporcodigo(View v) {
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "administracion", null, 1);
+        SQLiteDatabase bd = admin.getWritableDatabase();
+        String cod = et1.getText().toString();
+
+        Cursor fila = bd.rawQuery("select descripcion,precio from articulos where codigo=" + cod, null);
+
+        if(fila.moveToFirst()) {
+            et2.setText(fila.getString(0));
+            et3.setText(fila.getString(1));
+        } else
+            Toast.makeText(this, "No existe articulo", Toast.LENGTH_SHORT).show();
+        bd.close();
+    }
+
+    public void consultapordescripcion(View v) {
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "administracion", null, 1);
+        SQLiteDatabase bd = admin.getWritableDatabase();
+        String des = et2.getText().toString();
+
+        Cursor fila = bd.rawQuery("select codigo,precio from articulos where descripcion='" + des +"'", null);
+
+        if(fila.moveToFirst()) {
+            et1.setText(fila.getString(0));
+            et3.setText(fila.getString(1));
+        } else
+            Toast.makeText(this, "No existe articulo con descripcion", Toast.LENGTH_SHORT).show();
+        bd.close();
+    }
+
+    public void bajaporcodigo(View v) {
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "administracion", null, 1);
+        SQLiteDatabase bd = admin.getWritableDatabase();
+
+        String cod = et1.getText().toString();
+
+        int cant = bd.delete("articulos", "codigo=" + cod, null);
+        bd.close();
+
+        et1.setText("");
+        et2.setText("");
+        et3.setText("");
+
+        if (cant == 1)
+            Toast.makeText(this, "Se borró", Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(this, "No se borró, porqué no existe, melón!", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void modificacion(View v) {
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "administracion", null, 1);
+        SQLiteDatabase bd = admin.getWritableDatabase();
+
+        String cod = et1.getText().toString();
+        String descri = et2.getText().toString();
+        String pre = et3.getText().toString();
+
+        ContentValues registro = new ContentValues();
+        registro.put("codigo", cod);
+        registro.put("descripcion", descri);
+        registro.put("precio", pre);
+
+        int cant = bd.update("articulos", registro, "codigo=" + cod, null);
+        bd.close();
+
+        if (cant==1)
+            Toast.makeText(this, "Se actualizó", Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(this, "No se actualizó, porqué no existe, melón!", Toast.LENGTH_SHORT).show();
+
+    }
+}
